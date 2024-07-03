16d18db81f2dd29fa9e0674881b61e913a30db9f
Harshit Sachan <sachanharshit80@gmail.com>
2024-07-03 10:13:24 +0530

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
-
-
-
-
-
+app.use(express.json());
 let courses = [{ id : 1, name : "java"} ,
                 { id : 2, name : "node"} ,
                 { id : 3, name : "react"} ];
 app.get('/courses', (req, res) => {
    res.json(courses);
 });
+
+app.post('/courses', (req, res) => {
+    const course = {
+        id : courses.length + 1,
+        name : req.body.name
+    };
+    courses.push(course);
+    res.json(course);
+}
+);
 
 
 app.listen(3000, () => console.log('Server started'));
