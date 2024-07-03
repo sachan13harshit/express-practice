1ea59f1c7a93037ca9db762cad388be8d18885e4
Harshit Sachan <sachanharshit80@gmail.com>
2024-07-03 10:36:38 +0530

    first commit

--- index.js
+++ index.js
@@ -14,9 +14,25 @@ app.post('/courses', (req, res) => {
         name : req.body.name
     };
     courses.push(course);
+    res.save(course);
     res.json(course);
 }
 );
 
+app.put('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    course.name = req.body.name;
+    res.json(course);
+}
+);
+
+
+app.delete('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.json(course);
+}
+);
 
 app.listen(3000, () => console.log('Server started'));
